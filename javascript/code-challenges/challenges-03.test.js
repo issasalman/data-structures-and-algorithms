b554ce004e9c83c626e8532132fffa05dbe3e41a
issasalman <issasalmanhu1996@gmail.com>
2021-10-07 22:14:16 +0300

    done

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -30,6 +30,15 @@ const typeNum = (arr) => {
   return arr1;
 };
 
+// const typeNum = (arr) => {
+//   let arr1=arr.filter(element=>{
+
+// return typeof(element)==="number"
+
+//   })
+// return arr1
+// };
+
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
@@ -116,8 +125,14 @@ const snorlaxData = {
 };
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
-  // Solution code here...
-};
+  let arr1=arr.filter(element=>{
+      console.log(element.baseStat>minBaseStat);
+return (element.baseStat>minBaseStat)
+
+
+  })
+  return arr1
+ };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
@@ -264,7 +279,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return an array containing the stats that are greater than the input', () => {
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75)).toStrictEqual([ { stat: { url: 'https://pokeapi.co/api/v2/stat/5/', name: 'special-defense' }, effort: 2, baseStat: 110 } ]);
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75).length).toStrictEqual(1);
