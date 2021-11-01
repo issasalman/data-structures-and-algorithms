276fcf091e2a3734cc8439763511ebdefeb0f20e
issasalman <issasalmanhu1996@gmail.com>
2021-11-01 11:39:22 +0200

    edit whiteboard

--- python/code_challenges/array-reverse/array-reverse.py
+++ python/code_challenges/array-reverse/array-reverse.py
@@ -1,15 +1,22 @@
-arr1=list(input())
+arr1 = list(input())
+
 
 def reverseArray():
-
     """
     This function is to reverse any elements
     """
+    i = 0
 
-    print(arr1[::-1])
-    return arr1[::-1]
+    j = len(arr1)-1
+
+    while i < j:
+        arr1[i], arr1[j] = arr1[j], arr1[i]
+        i += 1
+        j -= 1
+
+        print(arr1)
+        return arr1
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     reverseArray()
-
